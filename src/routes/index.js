a1d07bcc2e1d373999f697dee4383f4f428ee975
andres <adpr@gmail.com>
2023-10-13 12:47:00 -0500

    routes/ index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
-// ARCHIVO ThemeRoutes.js
 import { useRoutes } from 'react-router-dom';
 import LoginRoutes from './LoginRoutes';
 import MainRoutes from './MainRoutes';
 import newRutes from './new_rutes';
 
-// ==============================|| ROUTING RENDER ||============================== //
-
 export default function ThemeRoutes() {
-  return useRoutes([LoginRoutes, newRutes, MainRoutes ]); // Cambia el orden de LoginRoutes y MainRoutes
+  return useRoutes([
+    LoginRoutes,
+    MainRoutes,
+    newRutes,
+  ]);
 }
