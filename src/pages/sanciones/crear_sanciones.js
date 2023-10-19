c13b538130a74f1c16fd0c31151f53948c11a4bb
marisol <ms763259@gmail.com>
2023-10-19 11:34:26 -0500

    crear_sanciones

--- src/pages/sanciones/crear_sanciones.js
+++ src/pages/sanciones/crear_sanciones.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import {
     Grid,
+    Stack,
+   InputLabel,
+   OutlinedInput,
 } from '@mui/material';
 
 const Crear_sanciones = () => {
     return (
         <form>
             <Grid container spacing={2}>
-                
+            <Grid item xs={12} md={12}>
+                    <Stack spacing={1}>
+                        <InputLabel htmlFor="descripicion">Descripcion</InputLabel>
+                        <OutlinedInput
+                            id="descripcion"
+                            type="string"
+                            name="descripcion"
+                            fullWidth
+                            required
+                        />
+                    </Stack>
+                </Grid>
             </Grid>
         </form>
     );
