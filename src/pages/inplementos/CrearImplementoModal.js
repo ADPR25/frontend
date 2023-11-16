eb15edc991b14aeccf88358f443310795ba32a9c
marisol <ms763259gmail.com>
2023-11-16 07:55:57 -0500

    cambio en implemento

--- src/pages/inplementos/CrearImplementoModal.js
+++ src/pages/inplementos/CrearImplementoModal.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Grid, Stack, DialogContent, Select, MenuItem, DialogActions, Dialog, DialogTitle, Button, InputLabel, OutlinedInput
+  Grid, Stack, Typography, IconButton, DialogContent, Select, MenuItem, DialogActions, Dialog, DialogTitle, Button, InputLabel, OutlinedInput
 } from '@mui/material';
-import { marca, categoria, C_implemento } from '../../api/crear_implemento.ts';
+import AddIcon from '@mui/icons-material/Add';
+import { marca, categoria, } from '../../api/crear_implemento.ts';
 import { estado_implemento } from '../../api/estado-implemento.ts';
 
 const CrearImplementoModal = ({ open, onClose }) => {
-  const [e_iData, sete_iData] = useState([]);
   const [marcaData, setMarcaData] = useState([]);
   const [categoriaData, setCategoriaData] = useState([]);
+  const [e_iData, sete_iData] = useState([]);
 
   const [formData, setFormData] = useState({
     codigo: '',
@@ -21,16 +22,13 @@ const CrearImplementoModal = ({ open, onClose }) => {
       detalle: '',
       tamano: '',
     },
-    categoria: [''],
+    categoria: '',
     cantidad: 0,
     img: null,
-    estado: [{
-      estado: '',
-      cantidad: 0,
-      apto: true,
-    }],
+    estado: [
+      { estado: '', cantidad: 0, apto: true },
+    ],
   });
-  
 
   useEffect(() => {
     async function fetchData() {
@@ -44,6 +42,8 @@ const CrearImplementoModal = ({ open, onClose }) => {
 
     fetchData();
   }, []);
+
+  const [cantidadSets, setCantidadSets] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
@@ -71,36 +71,70 @@ const CrearImplementoModal = ({ open, onClose }) => {
     fetchData();
   }, []);
 
-  const handleChange = (event) => {
+  const handleChange = (event, index) => {
     const { name, value } = event.target;
-  
+
     if (name === 'cantidad') {
       const valorNumerico = parseFloat(value);
-      setFormData({ ...formData, [name]: isNaN(valorNumerico) ? '' : valorNumerico });
+      setFormData((prevData) => ({
+        ...prevData,
+        cantidad: isNaN(valorNumerico) ? 0 : valorNumerico,
+      }));
     } else if (name.startsWith('descripcion')) {
       const campoDescripcion = name.split('.')[1];
       setFormData({
         ...formData,
         descripcion: { ...formData.descripcion, [campoDescripcion]: value || 'N/A' },
       });
-    } else if (name === 'estado') {
-      // Actualiza el campo "estado" para que sea un array de objetos
-      setFormData({
-        ...formData,
-        estado: [{
-          ...formData.estado[0],
-          estado: value || 'N/A',
-        }],
+    } else if (name.startsWith('estado')) {
+      setFormData((prevData) => {
+        const newEstado = prevData.estado || [];
+        return {
+          ...prevData,
+          estado: newEstado.map((e, i) =>
+            i === index ? { ...e, estado: value || 'N/A' } : e
+          ),
+        };
+      });
+    } else if (name.startsWith('cantidad')) {
+      setFormData((prevData) => {
+        const newEstado = prevData.estado || [];
+        return {
+          ...prevData,
+          estado: newEstado.map((e, i) =>
+            i === index ? { ...e, cantidad: value || 0 } : e
+          ),
+        };
+      });
+    } else if (name.startsWith('apto')) {
+      setFormData((prevData) => {
+        const newEstado = prevData.estado || [];
+        return {
+          ...prevData,
+          estado: newEstado.map((e, i) =>
+            i === index ? { ...e, apto: !e.apto } : e
+          ),
+        };
       });
     } else {
       setFormData({ ...formData, [name]: value || 'N/A' });
     }
   };
-  
-  ``
-  
-  
-  
+
+  const handleAddSet = () => {
+    setCantidadSets(cantidadSets + 1);
+    setFormData((prevData) => ({
+      ...prevData,
+      estado: [
+        ...prevData.estado,
+        {
+          estado: '',
+          cantidad: 0,
+          apto: true,
+        },
+      ],
+    }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -109,10 +143,10 @@ const CrearImplementoModal = ({ open, onClose }) => {
       console.error('La cantidad no es un número válido.');
       return;
     }
-    console.log('from data: ', formData)
 
     try {
-      const result = await C_implemento(formData);
+      // const result = await C_implemento(formData);
+      console.log(formData);
 
       console.log(result);
 
@@ -128,7 +162,7 @@ const CrearImplementoModal = ({ open, onClose }) => {
 
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Editar Sanción</DialogTitle>
+      <DialogTitle>Crear Implemento</DialogTitle>
       <DialogContent>
         <Grid container spacing={5} style={{ display: 'flex', justifyContent: 'space-between' }}>
           <Grid item xs={12} md={6}>
@@ -139,180 +173,193 @@ const CrearImplementoModal = ({ open, onClose }) => {
                 type="text"
                 name="codigo"
                 value={formData.codigo}
-                onChange={handleChange}
+                onChange={(e) => handleChange(e)}
                 fullWidth
                 style={{ width: '100%' }}
               />
             </Stack>
           </Grid>
 
-        
           <Grid item xs={12} md={6}>
-                        <Stack spacing={0}>
-                            <InputLabel htmlFor="nombre">Nombre</InputLabel>
-                            <OutlinedInput
-                                id="nombre"
-                                type="text"
-                                name="nombre"
-                                value={formData.nombre}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            />
-                        </Stack>
-                    </Grid>
-
-                    <Grid item xs={12} md={6}>
-                        <Stack spacing={0}>
-                            <InputLabel htmlFor="marca">Marca</InputLabel>
-                            <Select
-                                id="marca"
-                                name="marca"
-                                value={formData.marca}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            >
-                                {marcaData.map((option) => (
-                                    <MenuItem key={option._id} value={option._id}>
-                                        {option.nombre}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </Stack>
-                    </Grid>
-
-                    <Grid item xs={12} md={6}>
-                        <Stack spacing={0}>
-                            <InputLabel htmlFor="categoria">Categoría</InputLabel>
-                            <Select
-                                id="categoria"
-                                name="categoria"
-                                value={[formData.categoria]}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            >
-                                {categoriaData.map((option) => (
-                                    <MenuItem key={option._id} value={option._id}>
-                                        {option.nombre}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-
-                        </Stack>
-                    </Grid>
-
-                    <Grid item xs={12} md={12}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="descripcion">DESCRIPCION</InputLabel>
-                        </Stack>
-                    </Grid>
-
-                    <Grid item xs={12} md={4}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="peso">Peso</InputLabel>
-                            <OutlinedInput
-                                id="peso"
-                                name="descripcion.peso"
-                                value={formData.descripcion.peso}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            />
-                        </Stack>
-                    </Grid>
-                    
-                    <Grid item xs={12} md={4}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="color">Color</InputLabel>
-                            <OutlinedInput
-                                id="color"
-                                name="descripcion.color"
-                                value={formData.descripcion.color}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            />
-                        </Stack>
-                    </Grid>
-                    
-                    <Grid item xs={12} md={4}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="material">Material</InputLabel>
-                            <OutlinedInput
-                                id="material"
-                                name="descripcion.material"
-                                value={formData.descripcion.material}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            />
-                        </Stack>
-                    </Grid>
-                    
-                    <Grid item xs={12} md={6}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="detalle">Detalle</InputLabel>
-                            <OutlinedInput
-                                id="detalle"
-                                name="descripcion.detalle"
-                                value={formData.descripcion.detalle}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            />
-                        </Stack>
-                    </Grid>
-                    
-                    <Grid item xs={12} md={6}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="tamaño">Tamaño</InputLabel>
-                            <OutlinedInput
-                                id="tamano"
-                                name="descripcion.tamano"
-                                value={formData.descripcion.tamano}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            />
-                        </Stack>
-                    </Grid>
-
-                    <Grid item xs={12} md={6}>
-                        <Stack spacing={0}>
-                            <InputLabel htmlFor="cantidad">Cantidad</InputLabel>
-                            <OutlinedInput
-                                id="cantidad"
-                                type="number" // Asegura que el tipo sea numérico
-                                name="cantidad"
-                                value={formData.cantidad}
-                                onChange={handleChange}
-                                fullWidth
-                                style={{ width: '100%' }}
-                            />
-                        </Stack>
-                    </Grid>
+            <Stack spacing={0}>
+              <InputLabel htmlFor="nombre">Nombre</InputLabel>
+              <OutlinedInput
+                id="nombre"
+                type="text"
+                name="nombre"
+                value={formData.nombre}
+                onChange={(e) => handleChange(e)}
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Stack>
+          </Grid>
 
           <Grid item xs={12} md={6}>
             <Stack spacing={0}>
-              <InputLabel htmlFor="estado">Estado</InputLabel>
+              <InputLabel htmlFor="marca">Marca</InputLabel>
               <Select
-                id="estado"
-                name="estado"
-                value={formData.estado[0].estado}
-                onChange={handleChange}
+                id="marca"
+                name="marca"
+                value={formData.marca}
+                onChange={(e) => handleChange(e)}
                 fullWidth
                 style={{ width: '100%' }}
               >
-                {e_iData.map((option) => (
+                {marcaData.map((option) => (
                   <MenuItem key={option._id} value={option._id}>
-                    {option.estado}
+                    {option.nombre}
                   </MenuItem>
                 ))}
               </Select>
             </Stack>
           </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Stack spacing={0}>
+              <InputLabel htmlFor="categoria">Categoría</InputLabel>
+              <Select
+                id="categoria"
+                name="categoria"
+                value={formData.categoria}
+                onChange={(e) => handleChange(e)}
+                fullWidth
+                style={{ width: '100%' }}
+              >
+                {categoriaData.map((option) => (
+                  <MenuItem key={option._id} value={option._id}>
+                    {option.nombre}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={12}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="descripcion">DESCRIPCION</InputLabel>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={4}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="peso">Peso</InputLabel>
+              <OutlinedInput
+                id="peso"
+                name="descripcion.peso"
+                value={formData.descripcion.peso}
+                onChange={(e) => handleChange(e)}
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={4}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="color">Color</InputLabel>
+              <OutlinedInput
+                id="color"
+                name="descripcion.color"
+                value={formData.descripcion.color}
+                onChange={(e) => handleChange(e)}
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={4}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="material">Material</InputLabel>
+              <OutlinedInput
+                id="material"
+                name="descripcion.material"
+                value={formData.descripcion.material}
+                onChange={(e) => handleChange(e)}
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="detalle">Detalle</InputLabel>
+              <OutlinedInput
+                id="detalle"
+                name="descripcion.detalle"
+                value={formData.descripcion.detalle}
+                onChange={(e) => handleChange(e)}
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="tamaño">Tamaño</InputLabel>
+              <OutlinedInput
+                id="tamano"
+                name="descripcion.tamano"
+                value={formData.descripcion.tamano}
+                onChange={(e) => handleChange(e)}
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Stack>
+          </Grid>
+
+          {[...Array(cantidadSets)].map((_, index) => (
+            <React.Fragment key={index}>
+              <Grid item xs={12} md={6}>
+                <Stack spacing={0}>
+                  <InputLabel htmlFor={`estado-${index}`}>Estado</InputLabel>
+                  <Select
+                    id={`estado-${index}`}
+                    name={`estado-${index}`}
+                    value={formData.estado[index]?.estado || ''}
+                    onChange={(e) => handleChange(e, index)}
+                    fullWidth
+                    style={{ width: '100%' }}
+                  >
+                    {e_iData.map((option) => (
+                      <MenuItem key={option._id} value={option._id}>
+                        {option.estado}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </Stack>
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <Stack spacing={0}>
+                  <InputLabel htmlFor={`cantidad-${index}`}>Cantidad</InputLabel>
+                  <OutlinedInput
+                    id={`cantidad-${index}`}
+                    type="number"
+                    name={`cantidad-${index}`}
+                    value={formData.estado[index]?.cantidad || 0}
+                    onChange={(e) => handleChange(e, index)}
+                    fullWidth
+                    style={{ width: '100%' }}
+                  />
+                </Stack>
+              </Grid>
+            </React.Fragment>
+          ))}
+
+
+          <Grid item xs={6} md={6}>
+            <Typography variant="h5">
+              Cantidad:
+              <IconButton color="primary" aria-label="Añadir" onClick={handleAddSet}>
+                <AddIcon />
+              </IconButton>
+            </Typography>
+          </Grid>
+
         </Grid>
       </DialogContent>
       <DialogActions>
