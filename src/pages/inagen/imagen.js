d469dbd27128774fdb4619e47945c4e9effc6a4b
marisol <ms763259gmail.com>
2023-11-29 17:08:51 -0500

    staged

--- src/pages/inagen/imagen.js
+++ src/pages/inagen/imagen.js
@@ -1,7 +1,20 @@
+import { Grid,Stack,InputLabel,OutlinedInput } from "@mui/material";
+
 const Imagenes = () => {
     return (
         <>
-           
+             <Grid item xs={12} md={6}>
+            <Stack spacing={0}>
+              <InputLabel htmlFor="nombre">Nombre</InputLabel>
+              <OutlinedInput
+                id="nombre"
+                type="file"
+                name="nombre"
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Stack>
+          </Grid>
         </>
     );
 };
