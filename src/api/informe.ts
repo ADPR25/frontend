a48a8c829ad5801c6c00c1ffcff8fc117e2a1335
marisol <ms763259gmail.com>
2023-12-06 14:51:01 -0500

    cambio en la api de informe

--- src/api/informe.ts
+++ src/api/informe.ts
@@ -1,17 +1,29 @@
-    const API = 'https://proyecto-backend-sgbienestar.onrender.com/informe';
+const API = 'https://proyecto-backend-sgbienestar.onrender.com/informe';
 
-    export const informes = (informe) => {
-        return fetch(API, {
+export const informes = async (informe) => {
+    try {
+        const response = await fetch(API, {
             method: 'POST',
             body: JSON.stringify(informe),
             headers: {
-                'Content-Type': 'application/json', // Cambiado 'content-type' a 'Content-Type'
+                'Content-Type': 'application/json',
             },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error en la solicitud'); // Puedes personalizar el mensaje de error
-                }
-                return response.json();
-            });
-    };
+        });
+
+        if (!response.ok) {
+            // Si la respuesta no es exitosa, arrojamos un error
+            throw new Error('Error en la solicitud. Código de estado: ' + response.status);
+        }
+
+        const data = await response.json();
+
+        // Aquí puedes realizar más validaciones específicas de tu aplicación
+
+        return data;
+    } catch (error) {
+        console.error('Error en la función informes:', error);
+
+        // Devolvemos un objeto de error con un mensaje específico
+        return { error: 'Error al actualizar el archivo XLSX', details: error.message };
+    }
+};
