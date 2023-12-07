49958e13cd3c74159b6942963f640890e5dfd06a
marisol <ms763259gmail.com>
2023-12-07 14:41:23 -0500

    cambio en informe 2

--- src/pages/imformes/informe2.js
+++ src/pages/imformes/informe2.js
@@ -10,17 +10,18 @@ import {
     TextareaAutosize
 } from '@mui/material';
 import { informes } from '../../api/informe.ts';
-import {estado_implemento} from '../../api/estado-implemento.ts'
+import { estado_implemento } from '../../api/estado-implemento.ts';
 
 const Informes2 = () => {
-    
     const [formData, setFormData] = useState({
         tipo_informe: '65374b884d00eddbedad40fe',
-        usuario: '', 
+        usuario: '',
         dependencia: '',
         estado_implemento: [],
         observaciones: ''
     });
+
+    const [estadoData, setEstadoData] = useState([]); // Define estadoData state
 
     useEffect(() => {
         const loadUserId = async () => {
@@ -38,18 +39,16 @@ const Informes2 = () => {
         };
 
         loadUserId();
-    }, []); 
-    
-  
+    }, []);
+
     const handleFormChange = (event) => {
         const { name, value } = event.target;
 
-        // Special handling for 'estado_implemento' field
         if (name === 'estado_implemento') {
             const selectedValues = Array.isArray(value) ? value : [value];
             setFormData({
                 ...formData,
-                [name]: selectedValues.map(id => id._id),
+                [name]: selectedValues.map((id) => id._id),
             });
         } else {
             setFormData({
@@ -58,12 +57,12 @@ const Informes2 = () => {
             });
         }
     };
-    
+
     useEffect(() => {
         async function fetchData() {
             try {
                 const n_i = await estado_implemento();
-                setestadoData(n_i);
+                setEstadoData(n_i);
             } catch (error) {
                 console.error('Error al obtener los estados de los implementos', error);
             }
@@ -71,13 +70,12 @@ const Informes2 = () => {
         fetchData();
     }, []);
 
-
     const handleGuardarInforme = async () => {
         try {
             console.log(formData);
             const response = await informes(formData);
             console.log('Informe guardado con éxito:', response);
-    
+
             setFormData({
                 tipo_informe: '65374b884d00eddbedad40fe',
                 usuario: formData.usuario,
@@ -87,18 +85,17 @@ const Informes2 = () => {
             });
         } catch (error) {
             console.error('Error al guardar el informe:', error);
-    
-            // Log more details about the error
+
             if (error.response) {
                 console.error('Response data:', error.response.data);
             }
-    
-            // Handle specific errors based on your API response
+
             if (error.response && error.response.status === 400) {
                 console.error('Error 400: Bad Request');
             }
         }
     };
+
     return (
         <>
             <form>
@@ -116,27 +113,26 @@ const Informes2 = () => {
                         </Stack>
                     </Grid>
 
-                    <Grid item xs={12} md={6}>
-                            <Stack spacing={0}>
-                                <InputLabel htmlFor={`estado`}>Estado implemento</InputLabel>
-                                <Select
-                                    id={`estado`}
-                                    name={`estado_implemento`}
-                                    fullWidth
-                                    value={formData.estado_implemento}
-                                    onChange={(e) => handleFormChange(e)}
-                                >
-                                    {estadoData.map((option) => (
-                                        <MenuItem key={option._id} value={option._id}>
-                                            {option.estado}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </Stack>
-                        </Grid>
+                 <Grid item xs={12} md={6}>
+    <Stack spacing={0}>
+        <InputLabel htmlFor={`estado`}>Estado implemento</InputLabel>
+        <Select
+            id={`estado`}
+            name={`estado_implemento`}
+            fullWidth
+            value={formData.estado_implemento}  // Check the variable name here
+            onChange={(e) => handleFormChange(e)}
+        >
+            {estadoData.map((option) => (
+                <MenuItem key={option._id} value={option._id}>
+                    {option.estado}
+                </MenuItem>
+            ))}
+        </Select>
+    </Stack>
+</Grid>
 
-                   
-                    <Grid item xs={12} md={12}>     
+                    <Grid item xs={12} md={12}>
                         <Stack spacing={1}>
                             <InputLabel htmlFor="observaciones">Observaciones</InputLabel>
                             <TextareaAutosize
@@ -145,7 +141,7 @@ const Informes2 = () => {
                                 minRows={4}
                                 value={formData.observaciones}
                                 onChange={handleFormChange}
-                                placeholder='Descripción de la observacion'
+                                placeholder="Descripción de la observacion"
                                 style={{ width: '100%', height: '250px' }}
                             />
                         </Stack>
